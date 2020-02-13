976d290cd7a95ed8dfc1f5965a6415e4162ac9d5
FedericoOberndorfer <foberndorfer@eulidia.com>
2020-02-13 16:54:31 +0100

    Git Oauth functional version

--- run.bat
+++ run.bat
@@ -2,6 +2,4 @@ docker network create jupyterhub
 docker build -t myjupyterhub .
 docker pull jupyter/base-notebook
 
-call env.bat
-
 docker run --rm -it --env-file env.list -v /var/run/docker.sock:/var/run/docker.sock --net jupyterhub --name jupyterhub -p8000:8000 myjupyterhub jupyterhub
