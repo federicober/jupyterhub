c6bf57bc6d86f5437712eff484b0a4c311a54abf
FedericoOberndorfer <foberndorfer@eulidia.com>
2020-02-14 10:21:09 +0100

    datascience notebook

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 docker network create jupyterhub
 docker build -t myjupyterhub .
-docker pull jupyter/base-notebook
+docker pull jupyter/datascience-notebook
 
 docker run --rm -it --env-file env.list -v /var/run/docker.sock:/var/run/docker.sock --net jupyterhub --name jupyterhub -p8000:8000 myjupyterhub jupyterhub
